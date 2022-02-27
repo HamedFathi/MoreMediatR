3655c10ae96d219a6eca7a41e03325bf2a41f2dd
hamedfathi <hamedfathi@outlook.com>
2022-02-27 12:14:08 +0100

    feat(Behaviors): Logging & ExecutionTime added.

---
+++ MoreMediatR/Behaviors/ExecutionTimeBehavior.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MoreMediatR.Behaviors
+{
+    public class ExecutionTimeBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
+    {
+        private readonly ILogger<TRequest> _logger;
+
+        public ExecutionTimeBehavior(ILogger<TRequest> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            var requestName = request.GetType().Name;
+            TResponse response;
+            Stopwatch stopwatch = new Stopwatch();
+            try
+            {
+                stopwatch.Start();
+                response = await next();
+            }
+            finally
+            {
+                stopwatch.Stop();
+                _logger.LogInformation($"[Execution Time] {requestName} {stopwatch.ElapsedMilliseconds} Milliseconds.");
+            }
+            return response;
+        }
+    }
+}
---
+++ MoreMediatR/Behaviors/LoggingBehavior.cs
@@ -0,0 +1,41 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MoreMediatR.Behaviors
+{
+    public class LoggingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
+    {
+        private readonly ILogger<TRequest> _logger;
+
+        public LoggingBehavior(ILogger<TRequest> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            var requestName = request.GetType().Name;
+            var requestGuid = Guid.NewGuid().ToString();
+
+            var requestNameWithGuid = $"{requestName} [{requestGuid}]";
+
+            _logger.LogInformation($"[START] {requestNameWithGuid}");
+            TResponse response;
+
+            try
+            {
+                response = await next();
+            }
+            finally
+            {
+                _logger.LogInformation(
+                    $"[END] {requestNameWithGuid}");
+            }
+
+            return response;
+        }
+    }
+}
